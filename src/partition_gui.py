117a725e07cb455de977147848e8284de270fce2
Xiang Fan <sfanxiang@gmail.com>
2018-02-18 22:42:21 +0800

    partition_gui: update installation target message

--- src/partition_gui.py
+++ src/partition_gui.py
@@ -101,7 +101,9 @@ class PartitionGUI(GUIStep):
                     Gtk.DialogFlags.DESTROY_WITH_PARENT,
                     Gtk.MessageType.ERROR,
                     Gtk.ButtonsType.CLOSE,
-                    _('An installation target must be selected.')
+                    _('An installation target must be selected.') + '\n' +
+                        _('If not sure, choose an empty disk, and select ' +
+                            '"Automatically Set Up This Disk".')
                 )
                 dialog.run()
                 dialog.destroy()
