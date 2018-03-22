f48cd7e67a8ddeb35205a4065869902786dfea30
Xiang Fan <sfanxiang@gmail.com>
2018-03-22 12:46:15 +0800

    advanced_gui: add notice regarding where to change the values

--- src/advanced_gui.py
+++ src/advanced_gui.py
@@ -39,12 +39,14 @@ class AdvancedGUI(GUIStep):
             'clicked', self._keymap_reset_clicked)
 
     def update_text(self):
-        # TODO: add a notice regarding where to change the font and the keymap
         gui.builder.get_object('label_vm').set_label(_('VM Swappiness'))
         gui.builder.get_object('label_vm_swapping').set_label(_(
             'Note: On modern computers with large memory, VM swapping can ' +
             'slow the system down or even freeze the entire computer. Set ' +
-            'the value to 0 to disable swapping (does not affect hibernation).'
+            'the value to 0 to disable swapping (does not affect hibernation)' +
+            '.' +
+            '\n\n' +
+            'You can edit this value later in /etc/sysctl.d/99-sysctl.conf.'
         ))
         gui.builder.get_object('button_vm_reset').set_label(_('Reset'))
         gui.builder.get_object('label_vconsole').set_label(_('Virtual Console'))
@@ -52,6 +54,8 @@ class AdvancedGUI(GUIStep):
         gui.builder.get_object('button_font_reset').set_label(_('Reset'))
         gui.builder.get_object('label_vconsole_keymap').set_label(_('Keymap'))
         gui.builder.get_object('button_keymap_reset').set_label(_('Reset'))
+        gui.builder.get_object('label_vconsole_notice').set_label(_(
+            'You can edit these values later in /etc/vconsole.conf.'))
         gui.builder.get_object('label_advanced').set_label(_('Advanced'))
         gui.builder.get_object('label_advanced_notice').set_label(
             _('Edit these options only when you are familiar with them.'))
